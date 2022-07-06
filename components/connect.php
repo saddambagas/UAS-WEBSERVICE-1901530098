9d1acc29fb0c772e5448900c82cecf61179709e5
saddambagas <38762726+saddambagas@users.noreply.github.com>
2022-07-06 20:37:52 +0700

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql201.epizy.com;dbname=epiz_32097036_shop_db';
+$user_name = 'epiz_32097036';
+$user_password = 'Ov5zE36ZUt';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:+6281 237 613 192"><i class="fas fa-phone"></i> +6281 237 613 192</a>
+         <a href="tel:+6289 912 384 221"><i class="fas fa-phone"></i> +6289 912 384 221</a>
+         <a href="eshop@gmail.com"><i class="fas fa-envelope"></i> eshop@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Semarang, Indonesia - 50125 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>saddam bagas</span> | all rights reserved!</div>
+
+</footer>
